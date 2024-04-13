55a65f6363ddcdfd13b5eeac74e7d3e900a3ae62
[Abhishek Suwalka] <[abhisheksuwalka99502@gmail.com]>
2024-04-13 12:53:44 +0530

    uploading final code for calculator

--- 01-calculator/scripts.js
+++ 01-calculator/scripts.js
@@ -4,12 +4,13 @@ let currentEqn = "";
 let previousEqn = "";
 let justEvaluated = false;
 let previousAnswer = "";
+let clearButton = {};
 //   01 -> to change Theme .
 var changeTheme = document.getElementById("changeTheme");
 changeTheme.onclick = function () {
   document.body.classList.toggle("dark-theme");
 };
-////
+//
 
 //   Important Functions ...
 function updateDisplayValue() {
@@ -30,9 +31,9 @@ function addToDisplay(someValue) {
       someValue == "(" ||
       someValue == "ans";
     if (someValueIsNew) {
+      clearButton.value = "clr";
       currentEqn = "";
     }
-
     justEvaluated = false;
   }
   switch (someValue) {
@@ -66,19 +67,45 @@ function deleteFromEnd() {
   currentEqn = currentEqn.substring(0, currentEqn.length - 1);
   updateDisplayValue();
 }
-function clearDisplay() {
-  currentEqn = "";
-  updateDisplayValue();
+function clearDisplay(idValue) {
+  clearButton = document.getElementById(idValue);
+  if (currentEqn) {
+    // when currentEqn is not empty (first click.)
+    currentEqn = "";
+    clearButton.value = "AC";
+    updateDisplayValue();
+  } else {
+    // when currentEqn is empty (second click.)
+    currentEqn = "";
+    previousEqn = "";
+    justEvaluated = false;
+    clearButton = {};
+    updateDisplayValue();
+  }
 }
 
 /// for percentage % % %
-let evaluatingPercentage = false;
-let percentageValue = ""; // Variable to store the percentage value
-let resolvePreviousAnswer; // Promise resolver for the second number
+// let evaluatingPercentage = false;
+// let percentageValue = ""; // Variable to store the percentage value
+// let resolvePreviousAnswer; // Promise resolver for the second number
+// function percentageOf() {
+//   percentageValue = currentEqn; // Store the current equation as the percentage value
+//   currentEqn = ""; // Clear the current equation
+//   evaluatingPercentage = true;
+// }
 function percentageOf() {
-  percentageValue = currentEqn; // Store the current equation as the percentage value
-  currentEqn = ""; // Clear the current equation
-  evaluatingPercentage = true;
+  // percentageValue = currentEqn; // Store the current equation as the percentage value
+  // currentEqn = ""; // Clear the current equation
+  // evaluatingPercentage = true;
+  let finalValue;
+  try {
+    finalValue = eval(currentEqn);
+  } catch (error) {
+    console.log("Error in calculation");
+  }
+  currentEqn = finalValue / 100;
+  currentEqn = String(currentEqn);
+  updateDisplayValue();
 }
 
 function evaluateCurrentEqn() {
@@ -91,17 +118,19 @@ function evaluateCurrentEqn() {
     } else {
       previousAnswer = 0;
     }
-    if (evaluatingPercentage) {
-      // If in percentage calculation mode
-      let result = (parseFloat(percentageValue) * previousAnswer) / 100;
-      previousEqn = percentageValue + " % of " + previousAnswer;
-      currentEqn = String(result);
-      evaluatingPercentage = false; // Reset the flag
-    } else {
-      // If not in percentage calculation mode
-      currentEqn = String(previousAnswer);
-      previousEqn = previousEqn + " = " + currentEqn;
-    }
+    // if (evaluatingPercentage) {
+    //   // If in percentage calculation mode
+    //   let result = (parseFloat(percentageValue) * previousAnswer) / 100;
+    //   previousEqn = percentageValue + " % of " + previousAnswer;
+    //   currentEqn = String(result);
+    //   // evaluatingPercentage = false; // Reset the flag
+    // } else {
+    //   // If not in percentage calculation mode
+    //   currentEqn = String(previousAnswer);
+    //   previousEqn = previousEqn + " = " + currentEqn;
+    // }
+    currentEqn = String(previousAnswer);
+    previousEqn = previousEqn + " = " + currentEqn;
     updatePreviousEquation();
     updateDisplayValue();
   } catch (error) {
@@ -111,30 +140,102 @@ function evaluateCurrentEqn() {
 }
 
 function sqrRootThis() {
+  let squareRoot;
   try {
-    previousAnswer = eval(currentEqn);
+    squareRoot = eval(currentEqn);
   } catch (error) {
     console.log("Error in calculation");
   }
-  let squareRoot = Math.sqrt(previousAnswer);
+  squareRoot = Math.sqrt(squareRoot);
   currentEqn = String(squareRoot.toFixed(5).replace(/\.?0+$/, ""));
-  previousEqn = "square root of " + previousAnswer + " = " + currentEqn;
+  // previousEqn = "√" + previousAnswer + " = " + currentEqn;
   updateDisplayValue();
-  updatePreviousEquation();
+  // updatePreviousEquation();
 }
 
 function changeSignOfCurrentEqn() {
-  let currentEqnBackup = currentEqn;
+  // let currentEqnBackup = currentEqn;
+  let changedSign;
   try {
-    previousAnswer = eval(currentEqn)
+    changedSign = eval(currentEqn)
       .toFixed(12)
       .replace(/\.?0+$/, "");
   } catch (error) {
     console.log("Error in calculation");
   }
-  previousAnswer = -1 * previousAnswer;
-  previousEqn = "- ( " + currentEqnBackup + ") = " + previousAnswer;
-  currentEqn = String(previousAnswer);
+  changedSign = -1 * changedSign;
+  // previousEqn = "- ( " + currentEqnBackup + ") = " + previousAnswer;
+  currentEqn = String(changedSign);
   updateDisplayValue();
-  updatePreviousEquation();
+  // updatePreviousEquation();
 }
+
+// to edit keys design , when clicked
+document.addEventListener("DOMContentLoaded", function () {
+  var buttons = document.querySelectorAll(".keys");
+
+  buttons.forEach(function (button) {
+    button.addEventListener("mousedown", function () {
+      button.classList.add("clicked");
+    });
+
+    button.addEventListener("mouseup", function () {
+      button.classList.remove("clicked");
+    });
+
+    // mouseout event listener to handle the case when the mouse is dragged out of the button while still holding down
+    button.addEventListener("mouseout", function () {
+      button.classList.remove("clicked");
+    });
+  });
+});
+
+// KEYBOARD FUNCTIONALITY
+document.addEventListener("keydown", function (event) {
+  switch (event.key) {
+    case "Enter":
+    case "=":
+      evaluateCurrentEqn();
+      break;
+    case "e":
+      addToDisplay(Math.E);
+      break;
+    case "p":
+      addToDisplay(Math.PI);
+      break;
+    case "Backspace":
+      deleteFromEnd();
+      break;
+    case "%":
+      percentageOf();
+      break;
+    case "c":
+      clearDisplay();
+      break;
+    case "r":
+      sqrRootThis();
+      break;
+    case "0":
+    case "1":
+    case "2":
+    case "3":
+    case "4":
+    case "5":
+    case "6":
+    case "7":
+    case "8":
+    case "9":
+    case ".":
+    case "+":
+    case "-":
+    case "*":
+    case "/":
+    case "(":
+    case ")":
+      addToDisplay(event.key);
+      break;
+    default:
+      // Do nothing for other keys
+      break;
+  }
+});
